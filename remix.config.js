ceb8ad650754a67bcf1f9dde93f0f128394ae959
Cooksey <matthew.cooksey@circusstreet.com>
2025-03-27 15:07:31 +0000

    simplified project

---
+++ remix.config.js
@@ -0,0 +1,6 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  appDirectory: "src",  // or whatever your current source directory is
+  ignoredRouteFiles: ["**/.*"],
+  serverModuleFormat: "esm"
+} 
